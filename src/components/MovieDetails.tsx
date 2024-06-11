78a7a22b99ffd32038821c06be3316c36247f511
Daniel Leon <dalodev@proton.me>
2024-06-11 11:43:06 -0500

    Added title

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -84,6 +84,15 @@ function MovieDetails({
     getMovieDetails();
   }, [selectedId]);
 
+  useEffect(() => {
+    if (!title) return;
+    document.title = `Movie | ${title}`;
+
+    return () => {
+      document.title = "usePopcorn";
+    };
+  }, [title]);
+
   return (
     <div className="details">
       {isLoading && <Loader />}
